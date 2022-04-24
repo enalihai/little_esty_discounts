0b5ab1d3298bf7b1f97e3b6439139341cd75c268
Luke Pascale <lpluke5@gmail.com>
2022-04-24 15:58:20 -0500

    FeatTest: Setup feature test for merchant index

---
+++ spec/features/merchants/discounts/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Dashboard" do
+  before :each do
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+
+    @discount1 = @merchant1.discounts.create!(name: "Christmas", threshold: 10, percent: 15)
+    @discount2 = @merchant1.discounts.create!(name: "All Saint's Day", threshold: 15, percent: 20)
+    @discount3 = @merchant1.discounts.create!(name: "Diamond Monday", threshold: 20, percent: 25)
+
+    @discount4 = @merchant2.discounts.create!(name: "Regular Friday", threshold: 25, percent: 10)
+  end
+
+  it "has a link to a merchants discounts" do
+    visit "merchants/#{@merchant1.id}"
+
+    click_button("Promotions")
+    expect(current_path).to eq("/merchants/#{@merchant1.id}/discounts")
+  end
+
+  it 'displays all of the attributes of a discount' do
+    visit "/merchants/#{@merchant1.id}/discounts"
+
+    expect(page).to have_content(@discount1.name)
+    expect(page).to have_content(@discount1.threshold)
+    expect(page).to have_content(@discount1.percent)
+  end
+
+  it "has shows only that specific merchants discounts" do
+    visit "/merchants/#{@merchant1.id}/discounts"
+
+    expect(page).to have_content(@discount1.name)
+    expect(page).to have_content(@discount2.name)
+    expect(page).to have_content(@discount3.name)
+    expect(page).to_not have_content(@discount4.name)
+  end 
+end
