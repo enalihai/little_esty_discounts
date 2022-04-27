f8cd5908629c8b0700e3ad558f1fe1aad0a6a797
Luke Pascale <lpluke5@gmail.com>
2022-04-27 04:31:40 -0500

    Facade: Add facade folder and holiday_facade.rb

--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,5 +1,11 @@
 class HolidayService
-  def self.get_holidays
+  def self.holidays
     url = "https://date.nager.at/swagger/index.html"
-    
+    Faraday.new(url)
   end
+
+  def self.get_holidays
+    response = holidays.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
